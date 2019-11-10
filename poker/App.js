42385359fd023ab743ab789625d00ab9ad07d915
Joey Killam <jhkillam@gmail.com>
2019-11-10 16:25:56 -0600

    tweets rendering but not sorting

--- poker/App.js
+++ poker/App.js
@@ -2,7 +2,7 @@
 const App = (props) => {
     let { cards } = props;
     const CardsRender = props.cards.map(Card)
-    console.log('app CardsRender', CardsRender)
+    // console.log('app CardsRender', CardsRender)
     return (
         <span>{CardsRender}</span>
     )
@@ -11,7 +11,7 @@ const App = (props) => {
 // CHALLENGE: Write a separate Card component for use in the App component
 // TIP: Use props to pass the suit and number to each Card component
 const Card = (props) => {
-    console.log("Card component props:", props)
+    // console.log("Card component props:", props)
     let url = './cards/' + props.value + props.suit + '.png'
     return (
         <img src={url} width="151"></img>
--- tweets/App.js
+++ tweets/App.js
@@ -1,14 +1,51 @@
 // Render the tweets using React!
 const App = (props) => {
     let { tweets } = props;
+    // console.log('app props', props)
+    const TweetsRender = props.tweets.map(Tweet)
     return (
-        <span>tweets</span>
+        <div>
+            {TweetsRender}
+        </div>
     )
 }
 
-const Tweet = null;  // CHALLENGE: Write a separate Tweet component for use in the App component
+const Tweet = (props) => {
+    // console.log('Tweet component props:', props)
+    let url = props.user.profilePic
+    let verifiedCheck = ""
+    if (props.user.isVerified) {
+        verifiedCheck = <img src="./twitterIcons/check-circle.svg" />
+    }
+    const FlexStyle = {
+        display: "flex",
+        justifyContent: "space-evenly" 
+    }
+    const IconPadding = {
+        paddingRight: "10px"
+    }
+    return (
+        <div className="card">
+            <div className="card-body">
+                <img width="50" src={url} />
+                <h5 className="card-title">{props.user.handle} {verifiedCheck}</h5>
+                <p className="card-text">{props.text}</p>
+                <div style={FlexStyle}>
+                    <span><img style={IconPadding} src="./twitterIcons/message-circle.svg"/>{props.replies}</span>
+                    <span><img style={IconPadding} src="./twitterIcons/repeat.svg"/>{props.retweets}</span>
+                    <span><img style={IconPadding} src="./twitterIcons/heart.svg"/>{props.likes}</span>
+                </div>
+            </div>
+        </div>
+    )
+};  // CHALLENGE: Write a separate Tweet component for use in the App component
 
-const User = null;  // CHALLENGE: Write a separate User component for use in the Tweet component
+// const User = (props) => {
+//     return (
+//         <span>{props.user.handle}</span>
+//     )
+// };  
+// CHALLENGE: Write a separate User component for use in the Tweet component
 
 const Metrics = null;  // CHALLENGE: Write a separate Metrics component for use in the Tweet component (likes, retweets, replies)
 
